ffda6d437a9d048d07a32e7f56c7cda16399b619
Антон Васин <mail@vasin.space>
2016-12-28 21:59:06 +0300

    Derivatives calculations

--- constrained-dynamics/app.js
+++ constrained-dynamics/app.js
@@ -1,66 +1,111 @@
 'use strict';
 
-function Vector(){
+function Vector() {
     Array.call(this);
     this.length = arguments.length;
-    for(var i = 0; i < arguments.length; ++i)
+    for (var i = 0; i < arguments.length; ++i)
         this[i] = arguments[i];
 }
 
 Vector.prototype = Object.create(Array.prototype);
 Vector.prototype.constructor = Vector;
 
-Vector.prototype.add = function(v){
-    if(v.length != this.length)
+Vector.prototype.add = function (v) {
+    if (v.length != this.length)
         throw new RangeError();
 
-    return this.map((e, i)=> e + v[i]);
-};
-
-Vector.prototype.sub = function(v){
-    if(v.length != this.length)
-        throw new RangeError();
-
-    return this.map((e, i)=> e - v[i]);
-};
-
-Vector.prototype.scale = function(f){
-    return this.map(e => e * f);
-};
-
-Vector.prototype.dot = function(v){
-    if(v.length != this.length)
-        throw new RangeError();
-
-    return this.reduce((sum, e, i) => sum + e*v[i], 0);
-};
-
-Vector.prototype.magnitude = function(){
-    return Math.sqrt(this.dot(this));
-};
-
-Vector.prototype.clone = function(){
     var result = new Vector();
     result.length = this.length;
-    this.forEach((e,i) => result[i] = e);
+    this.forEach((e, i) => result[i] = e + v[i]);
     return result;
 };
 
-function derivative(func, arg, epsilon){
-    if(epsilon === undefined)
+Vector.prototype.sub = function (v) {
+    if (v.length != this.length)
+        throw new RangeError();
+
+    var result = new Vector();
+    result.length = this.length;
+    this.forEach((e, i) => result[i] = e - v[i]);
+    return result;
+};
+
+Vector.prototype.scale = function (f) {
+    var result = new Vector();
+    result.length = this.length;
+    this.forEach((e, i) => result[i] = e * f);
+    return result;
+};
+
+Vector.prototype.dot = function (v) {
+    if (v.length != this.length)
+        throw new RangeError();
+
+    return this.reduce((sum, e, i) => sum + e * v[i], 0);
+};
+
+Vector.prototype.magnitude = function () {
+    return Math.sqrt(this.dot(this));
+};
+
+Vector.prototype.clone = function () {
+    var result = new Vector();
+    result.length = this.length;
+    this.forEach((e, i) => result[i] = e);
+    return result;
+};
+
+function derivative(func, arg, epsilon) {
+    if (epsilon === undefined)
         epsilon = 1e-6;
 
-    if(initialValue.constructor == Number){
-        if(arg.constructor == Number)
-            return (func(arg + epsilon) - func(arg + epsilon))/(2*epsilon);
-
-        if(arg.constructor == Vector){
-            
-        }
+    if (arg.constructor == Number) {
+        let left = func(arg - epsilon);
+        let right = func(arg + epsilon);
+        if (left.constructor == Number && right.constructor == Number)
+            return (right - left) / (2 * epsilon)
+        else if (left.constructor == Vector && right.constructor == Vector)
+            return right.sub(left).scale(0.5 / epsilon);
+        else
+            throw new TypeError("not implemented yet");
     }
-    else if(initialValue.constructor == Vector){
+    else if (arg.constructor == Vector) {
+        return arg.map((value, i) => {
+            let left = arg.clone();
+            let right = arg.clone();
+            left[i] -= epsilon;
+            right[i] += epsilon;
+
+            left = func(left);
+            right = func(right);
+
+            if (left.constructor == Number && right.constructor == Number)
+                return (right - left) / (2 * epsilon);
+            else if (left.constructor == Vector && right.constructor == Vector)
+                return right.sub(left).scale(0.5 / epsilon);
+            else
+                throw new TypeError("not implemented yet");
+        });
 
     }
     else
         throw new TypeError("not implemented yet");
 }
+
+function Sphere(q) {
+    return q.magnitude() - 5;
+}
+
+function Plane(q) {
+    const normal = new Vector(0, 1, 0);
+    return q.dot(normal);
+}
+
+function C(q) {
+    return new Vector(Sphere(q), Plane(q));
+}
+
+const q = new Vector(0, 0, 5);
+
+var JacobianMat = derivative(C, q);
+console.log(JacobianMat);
